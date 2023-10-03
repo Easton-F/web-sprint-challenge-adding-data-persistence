952bcb86808c94c29701f0fed32dd0f9c1910328
Easton <easton.fritsch21@gmail.com>
2023-10-03 13:16:56 -0600

    linked routers to server.js

--- api/server.js
+++ api/server.js
@@ -1,7 +1,17 @@
 // build your server here and require it from index.js
 const express = require('express')
 const server = express()
+const projectsRouter = require('./project/router')
+const resourcesRouter = require('./resource/router')
+const tasksRouter = require('./task/router')
 
 server.use(express.json())
+server.use('/api/projects', projectsRouter)
+server.use('/api/resources', resourcesRouter)
+server.use('/api/tasks', tasksRouter)
+
+server.use('*', (req, res) => {
+    res.json({ api: 'up' })
+})
 
 module.exports = server;
