75b4f06bbf45c757cbfe6ff437cd3405011a5379
Easton <easton.fritsch21@gmail.com>
2023-10-03 13:38:09 -0600

    wip project-router get

--- api/project/router.js
+++ api/project/router.js
@@ -1,12 +1,17 @@
 // build your `/api/projects` router here
 const router = require('express').Router()
+const Projects = require('./model')
 
 router.post('/', (req, res, next) => {
 
 })
 
 router.get('/', (req, res, next) => {
-
+    Projects.getAllProjects()
+        .then(result => {
+            res.status(200).json(result)
+        })
+        .catch(next)
 })
 
 module.exports = router;
