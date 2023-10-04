98d4245ae86eade79a7ea6d2e32d7627d0aa909c
Easton <easton.fritsch21@gmail.com>
2023-10-03 19:43:59 -0600

    finished projects router and model

--- api/project/model.js
+++ api/project/model.js
@@ -6,6 +6,21 @@ function getAllProjects() {
         .select('pj.*')
 }
 
+function addNewProject(newProject) {
+    return db('projects')
+        .insert(newProject)
+            .then(val  => {
+                return getProjectById(val)
+            })
+}
+
+function getProjectById(id) {
+    return db('projects as pj')
+        .where('project_id', id)
+        .select('pj.*')
+}
+
 module.exports = {
     getAllProjects,
+    addNewProject,
 }
--- api/project/router.js
+++ api/project/router.js
@@ -3,12 +3,33 @@ const router = require('express').Router()
 const Projects = require('./model')
 
 router.post('/', (req, res, next) => {
+    const { project_name, project_completed, project_description } = req.body
 
+    if (!project_name){
+        res.status(400).json({
+            message: "must have a project_name",
+        })
+    }
+
+    Projects.addNewProject({ project_name, project_completed: project_completed === null ||  project_completed === undefined ? 0 : project_completed, project_description })
+        .then(newProject => {
+                res.json({
+                    ...newProject[0],
+                    project_completed: newProject[0].project_completed === 0 ? false : true
+                })
+        })
+        .catch(next)
 })
 
 router.get('/', (req, res, next) => {
     Projects.getAllProjects()
         .then(result => {
+            result = result.map((value) => {
+                return {
+                    ...value,
+                    project_completed: value.project_completed === 0 ? false : true
+                }
+            }) 
             res.status(200).json(result)
         })
         .catch(next)
