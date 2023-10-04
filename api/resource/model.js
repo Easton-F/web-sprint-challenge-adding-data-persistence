1a127e1ec7d4f291fa215b6089cf194ed3437dff
Easton <easton.fritsch21@gmail.com>
2023-10-03 19:56:24 -0600

    finished resources router and model

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,26 @@
 // build your `Resource` model here
+const db = require('../../data/dbConfig')
+
+function getAllResources() {
+    return db('resources as rc')
+        .select('rc.*')
+}
+
+function addNewResource(newResource) {
+    return db('resources')
+        .insert(newResource)
+            .then(val  => {
+                return getResourceById(val)
+            })
+}
+
+function getResourceById(id) {
+    return db('resources as rc')
+        .where('resource_id', id)
+        .select('rc.*')
+}
+
+module.exports = {
+    getAllResources,
+    addNewResource,
+}
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,12 +1,29 @@
 // build your `/api/resources` router here
 const router = require('express').Router()
+const Resources = require('./model')
 
 router.post('/', (req, res, next) => {
+    const { resource_name, resource_description } = req.body
 
+    if (!resource_name){
+        res.status(400).json({
+            message: "must have a resource_name",
+        })
+    }
+
+    Resources.addNewResource({ resource_name, resource_description })
+        .then(newResource => {
+                res.json(newResource[0])
+        })
+        .catch(next)
 })
 
 router.get('/', (req, res, next) => {
-
+    Resources.getAllResources()
+    .then(result => {
+        res.status(200).json(result)
+    })
+    .catch(next)
 })
 
 module.exports = router;
